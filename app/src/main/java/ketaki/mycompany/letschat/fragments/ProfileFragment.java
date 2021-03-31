dc2358fe342f7b57bf568be428b8cfd15da6de20
Ketaki-Gangadhar <ketakigangadhar0811@gmail.com>
2021-03-31 13:59:27 +0530

    updated chat app

--- app/src/main/java/ketaki/mycompany/letschat/fragments/ProfileFragment.java
+++ app/src/main/java/ketaki/mycompany/letschat/fragments/ProfileFragment.java
@@ -1,17 +1,29 @@
 package ketaki.mycompany.letschat.fragments;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -19,10 +31,18 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
 
 import ketaki.mycompany.letschat.MessageActivityy;
 import ketaki.mycompany.letschat.Model.MyUsers;
 import ketaki.mycompany.letschat.R;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class ProfileFragment extends Fragment {
@@ -32,29 +52,36 @@ public class ProfileFragment extends Fragment {
 
     DatabaseReference reference;
     FirebaseUser fUser;
+
+    //profile image
+    StorageReference storageReference;
+    private static final int IMAGE_REQ = 1;
+    private Uri imageUri;
+    private StorageTask uploadTask;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-      View view = inflater.inflate(R.layout.fragment_profile,container,false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-      userName = view.findViewById(R.id.profile_Username_User);
-      profileImage = view.findViewById(R.id.profile_Image_User);
+        userName = view.findViewById(R.id.profile_Username_User);
+        profileImage = view.findViewById(R.id.profile_Image_User);
+
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
+
 
         fUser = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference("MyUsers")
-        .child(fUser.getUid());
+                .child(fUser.getUid());
 
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 MyUsers user = snapshot.getValue(MyUsers.class);
                 userName.setText(user.getUsername());
-                if(user.getImageURL().equals("default"))
-                {
+                if (user.getImageURL().equals("default")) {
                     profileImage.setImageResource(R.drawable.person);
-                }
-                else
-                {
+                } else {
                     Glide.with(getContext()).load(user.getImageURL()).into(profileImage);
                 }
 
@@ -66,6 +93,104 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent();
+                i.setType("image/");
+                i.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(i, IMAGE_REQ);
+            }
+        });
+
         return view;
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver contentResolver = getContext().getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void UploadMyImage() {
+        final ProgressDialog progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Uploading..");
+        progressDialog.show();
+
+        if (imageUri != null) {
+            final StorageReference fileRef = storageReference.child(System.currentTimeMillis()
+                    + "." + getFileExtension(imageUri));
+
+            uploadTask = fileRef.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+
+                    if(!task.isSuccessful())
+                    {
+                        throw task.getException();
+                    }
+                    return fileRef.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+
+                    if(task.isSuccessful())
+                    {
+                        Uri downloadUri = task.getResult();
+                        String mUri = downloadUri.toString();
+
+                        reference = FirebaseDatabase.getInstance().getReference("MyUsers").child(fUser.getUid());
+
+                        HashMap<String , Object>map = new HashMap<>();
+
+                        map.put("imageURL",mUri);
+                        reference.updateChildren(map);
+
+                        progressDialog.dismiss();
+                    }else
+                    {
+                        Toast.makeText(getContext(), "Failed!", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                }
+            });
+        }
+        else
+        {
+            Toast.makeText(getContext(), "No Image Selected", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+          if(requestCode == IMAGE_REQ && resultCode == RESULT_OK
+          && data!=null && data.getData() != null)
+          {
+              imageUri = data.getData();
+
+              if(uploadTask != null && uploadTask.isInProgress())
+              {
+                  Toast.makeText(getContext(), "Upload In Progress..", Toast.LENGTH_SHORT).show();
+              }
+              else
+              {
+                  UploadMyImage();
+              }
+          }
     }
 }
